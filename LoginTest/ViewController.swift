3dbafa27d89c15396b43a8ad7937c79f59eaf53a
Emayoung <emayoung95@gmail.com>
2019-10-09 22:55:11 +0100

    Initial Commit

---
+++ LoginTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginTest
+//
+//  Created by Ememobong Akpanekpo on 09/10/2019.
+//  Copyright © 2019 Ememobong Akpanekpo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
