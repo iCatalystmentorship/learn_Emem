01a79fdd4e6b33661c3c18a3168e04ae61e87cfd
Emayoung <emayoung95@gmail.com>
2019-10-10 02:19:03 +0100

    Initial commit - starter.

---
+++ LoginTest/View/DesignableUITextField.swift
@@ -0,0 +1,70 @@
+//
+//  DesignableUITextField.swift
+//  LoginTest
+//
+//  Created by Ememobong Akpanekpo on 10/10/2019.
+//  Copyright © 2019 Ememobong Akpanekpo. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableUITextField: UITextField {
+    
+    // Provides left padding for images
+    override func leftViewRect(forBounds bounds: CGRect) -> CGRect {
+        var textRect = super.leftViewRect(forBounds: bounds)
+        textRect.origin.x += leftPadding
+        return textRect
+    }
+    
+    @IBInspectable var leftImage: UIImage? {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable var leftPadding: CGFloat = 0
+    
+    @IBInspectable var color: UIColor = UIColor.lightGray {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0{
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    func updateView() {
+        if let image = leftImage {
+            leftViewMode = UITextField.ViewMode.always
+            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 78, height: 78))
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = image
+            // Note: In order for your image to use the tint color, you have to select the image in the Assets.xcassets and change the "Render As" property to "Template Image".
+            imageView.tintColor = color
+            leftView = imageView
+        } else {
+            leftViewMode = UITextField.ViewMode.never
+            leftView = nil
+        }
+        
+        // Placeholder text color
+        attributedPlaceholder = NSAttributedString(string: placeholder != nil ?  placeholder! : "", attributes:[NSAttributedString.Key.foregroundColor: color])
+    }
+}
--- LoginTest/ViewController.swift
+++ LoginTest/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
