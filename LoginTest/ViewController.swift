ef1052c792137be2367aca5be9d6c4e363c6b459
Emayoung <emayoung95@gmail.com>
2019-10-22 00:46:54 +0100

    Added Google Login and facebook login

--- LoginTest/ViewController.swift
+++ LoginTest/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
+import FacebookLogin
 
-class ViewController: UIViewController, UITextFieldDelegate {
+
+class ViewController: UIViewController, UITextFieldDelegate, GIDSignInDelegate {
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        
+        
+    }
     
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var userNameTextField: DesignableUITextField!
     @IBOutlet weak var passwordTextField: DesignableUITextField!
+    @IBOutlet weak var googleSignInButton: GIDSignInButton!
     
     var activeTextField: DesignableUITextField?
     var lastOffSet = 0
@@ -22,10 +32,19 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Do any additional setup after loading the view.
         self.userNameTextField.delegate = self
         self.passwordTextField.delegate = self
+        
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+        
+        // Automatically sign in the user if signed in already
+        GIDSignIn.sharedInstance()?.restorePreviousSignIn()
+        
+        
     }
     
     @IBAction func login(_ sender: Any) {
-        showAlertMessge(message: "Logged in successful 👍")
+        let email = userNameTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        signIn(email, password)
     }
     
     @IBAction func signUp(_ sender: Any) {
@@ -56,5 +75,49 @@ class ViewController: UIViewController, UITextFieldDelegate {
         return true
     }
     
+    func signIn(_ email: String, _ password: String){
+        
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+            guard let strongSelf = self else { return }
+        
+            // ...
+            if let user = authResult?.user {
+                self?.showAlertMessge(message: "Login successful 👍")
+            } else {
+                self?.showAlertMessge(message: "Login failed 😢")
+            }
+        }
+    }
+    
+    
+    @IBAction func facebookSignIn(_ sender: Any) {
+        let loginManager = LoginManager()
+        loginManager.logIn(
+            permissions: [.publicProfile, .userFriends],
+            viewController: self
+        ) { result in
+            self.loginManagerDidComplete(result)
+        }
+    }
+    
+    
+    @IBAction func googleSignIn(_ sender: Any) {
+        
+    }
+    
+    
+    func loginManagerDidComplete(_ result: LoginResult) {
+        let alertController: UIAlertController
+        switch result {
+        case .cancelled:
+            showAlertMessge(message: "User cancelled login.")
+            
+        case .failed(let error):
+            showAlertMessge(message: "Login failed with error \(error)")
+            
+        case .success(let grantedPermissions, _, _):
+            showAlertMessge(message: "Login succeeded with granted permissions: \(grantedPermissions)")
+        }
+    }
 }
 
